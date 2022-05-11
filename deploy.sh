871d6dc30fadad0f77465bc5460ed720768f1225
Gabriel S. S. Costa <gabrielssc.ti@gmail.com>
2022-05-11 12:56:13 -0300

    AWS Lambda Hello World!

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Build the binary"
+GOOS=linux GOARCH=amd64 go build -o main main.go
+echo "Create a ZIP file"
+zip deployment.zip main
+echo "Cleaning up"
+rm main
+
---
+++ go.mod
@@ -0,0 +1,5 @@
+module aws-lambdas-practice
+
+go 1.18
+
+require github.com/aws/aws-lambda-go v1.31.1 // indirect
